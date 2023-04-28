a02aea14abcd29a7b83680e01a553b76fc2dcb5f
Judah <judahben149@gmail.com>
2023-04-28 16:40:09 +0100

    Added app string constants

---
+++ app/src/main/java/com/judahben149/paging3trial/Constants.kt
@@ -0,0 +1,10 @@
+package com.judahben149.paging3trial
+
+object Constants {
+
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "30239e3691bfcb7c062c8383a63cf7a9"
+    const val BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/"
+
+    const val TAG_API = "apii"
+}
